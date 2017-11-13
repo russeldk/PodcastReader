8118f687bbf3101c440ce60611188f216f1f034a
Rune Russel <runerussel@gmail.com>
2017-11-13 23:28:23 +0100

    NUnit + test

---
+++ PodcastReader.Tests/Controllers/PodcastControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PodcastReader.Controllers;
+using PodcastReader.Repository.Podcasts;
+using System.Web.Mvc;
+
+namespace PodcastReader.Tests.Controllers
+{
+    [TestFixture]
+    public class PodcastControllerTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            AutoMapperConfiguration.Configure();
+        }
+
+        [Test]
+        public void IndexTest()
+        {
+            PodcastController controller = new PodcastController(new PodcastRepository());
+            
+            ViewResult result = controller.Index() as ViewResult;
+            
+            Assert.IsNull(result);
+        }
+    }
+}
